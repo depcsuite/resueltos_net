da78f568504a75d8298326e7a52f27a20f5d86f0
CMES Software <cmessotftware@gmail.com>
2023-01-17 21:14:41 -0300

    clase 17.01.2023

---
+++ 4-ASP-Net MVC Core/00- TiendaLibros (clase 17.01.2023)/TiendaLibros/Models/Libro.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace TiendaLibros.Models
+{
+    public class Libro
+    {
+        public string Isbn { get; set; }
+
+        [Display(Name = "Libro Title")]
+        [Required]
+        public string Titulo { get; set; }
+
+        public string Autor { get; set; }
+
+        public string Genero { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Range(1, 10000)]
+        public decimal Precio { get; set; }
+
+        public string Resumen { get; set; }
+
+        public string PortadaUri { get; set; }
+
+        [Display(Name = "Dia de publicación")]
+        [DataType(DataType.Date)]
+        public DateTime FechaPublicacion { get; set; }
+
+        public Detalle Detalles { get; set; }
+    }
+}
