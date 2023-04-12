88274ab14ae2a66b09bc9b7534a6f9dde9882b2b
CMES Software <cmessotftware@gmail.com>
2023-04-11 21:18:39 -0300

    mas casos de test

--- 8-JavaScript/Testing/Ejemplo1/src/array.js
+++ 8-JavaScript/Testing/Ejemplo1/src/array.js
@@ -1,20 +1,94 @@
 export function sumar(arr){
-  arr.reduce( (total, num ) => {
-    console.log(total);
-    return total + num;
-  },0)
+  return (
+    arr.reduce( (acum, item ) =>  {
+      return acum + item;
+  },0))
 }
 
-export function dividir(a,b) {
-  return a/b;
+export function sumar_indice_pares(arr){
+  return (
+    arr.reduce( (acum, item , index) => {
+      return index%2 == 0 ? acum + item : acum + 0;
+  },0))
 }
 
-export function multiplicar(a,b) {
-  return a*b;
+
+//Usar sort
+export function ordenar(arr) {
+  
 }
 
-export function modulo(a,b) {
-  return a%b;
+//Usar reverse
+export function alrevez(arr) {
+  
+}
+
+//Usar push
+export function agregar(arr, elemento) {
+ 
+}
+
+export function extraer(arr) {
+ 
+}
+
+//Usar indeOf
+export function buscar(arr, elemento) {
+ 
+}
+
+//Usar filter
+export function filtar(arr, filtro) {
+ 
+}
+
+//Usar map
+export function mapear_strings (arr, condicion ) {
+ 
+}
+
+//Usar split
+export function separar (arr, separador ) {
+ 
+}
+
+//Usar join
+export function unir (cadena, separador ) {
+ 
+}
+
+//Usar foreEach
+export function duplicar (arr) {
+ 
+}
+
+//Usar map
+export function duplicar2 (arr) {
+ 
+}
+
+export function combinar (arr1, arr2 ) {
+ 
+}
+
+//Separar en dos partes, primera cumple condicion y 
+//la segunda parte no la cumple.
+export function separar_array (arr,condicion) {
+ 
+}
+
+//arr es una array de objetos por fecha {int id, string nombre, date fecha}
+export function ordenar_por_fecha (arr) {
+ 
+}
+
+//arr es una array de objetos por nombre {int id, string nombre, date fecha}
+export function ordenar_por_nombre (arr) {
+ 
+}
+
+export function extraer_resto(arr){
+
 }
 
 
--- 8-JavaScript/Testing/Ejemplo1/test/test.array.js
+++ 8-JavaScript/Testing/Ejemplo1/test/test.array.js
@@ -1,4 +1,5 @@
-import equal from "assert";
+import { equal } from "assert";
+
 import {
   sumar,
   agregar,
@@ -14,7 +15,10 @@ import {
   ordenar_por_nombre,
   separar,
   separar_array,
-  unir
+  unir,
+  sumar_indice_pares,
+  extraer_resto,
+  duplicar2
 } from "../src/array.js";
 
 
@@ -24,8 +28,41 @@ console.log(arr);
 
 describe('Operaciones con arrays', () => {
   it('sumar elementos', () => {
-    equal(sumar(arr), 0);
+    equal(sumar(arr), 13);
   });
+  it('sumar elementos de indice par', () => {
+    equal(sumar_indice_pares(arr), 1);
+  });
+  it('agregar elementos al array', () => {
+    equal(agregar(arr,33), [1, 4, 5, -2, 6, 10, -11,33]);
+  });
+  it('Da vuelta los elementos al array', () => {
+    equal(alrevez(arr), [-11,10,6,-2,5,4,1]);
+  });
+  it('buscar un elemento -> devuelve el index de la primera ocurrencia',
+   () => {
+    equal(buscar(arr,10), 5);
+  });
+  it('buscar un elemento -> devuelve -1 si no lo encuentra',
+   () => {
+    equal(buscar(arr,22), -1);
+  });
+  it('combinar dos arreglos',  () => {
+   let arr1 = [11,22,33,44,55,66]
+   equal(combinar(arr,arr1), [1, 4, 5, -2, 6, 10, -11,11,22,33,44,55,66]);
+ });
+ it('extraer primer elemento del array', () => {
+  equal(extraer(arr), 1);
+});
+  it('extrae primer elemento del array y devuelve el resto', () => {
+    equal(extraer_resto(arr), [4, 5, -2, 6, 10, -11]);
+});
+it('duplicar cada elemento del array (foreEach)', () => {
+  equal(duplicar(arr), [4, 5, -2, 6, 10, -11]);
+});
+it('duplicar2 cada elemento del array (map)', () => {
+  equal(duplicar2(arr), [4, 5, -2, 6, 10, -11]);
+});
 });
 
 
